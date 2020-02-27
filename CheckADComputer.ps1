beb81356e26b1dfa725c746ceb26747ef94eff37
PLUSInfra <infra@plusretail.nl>
2020-02-27 16:35:46 +0100

    Add files via upload

---
+++ CheckADComputer.ps1
@@ -0,0 +1,5 @@
+﻿$computers = "C:\Beheer\Scripts\ADComputer\input.txt" 
+ 
+$ADComp = "C:\Beheer\Scripts\ADComputer\output.txt/.Log>" 
+ 
+Get-Content $computers | Foreach {If (Get-ADComputer -Filter {Name -eq $_}) {Add-Content $ADComp "$_ is Enabled $((Get-ADComputer $_).enabled) in Active Directory."} Else {Add-Content $ADComp "Warning!! $_ is not in Active Directory."}}
